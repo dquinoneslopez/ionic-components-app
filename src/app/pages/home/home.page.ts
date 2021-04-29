5f6834fe1eae95fe0756ee60c00789f74103de07
dquinoneslopez <dquinoneslopez@gmail.com>
2021-04-29 10:47:23 +0200

    First commit.

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Componente } from 'src/app/interfaces/components';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  
+  components: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
