94d0586356fb252eac9a1ea88a2b161b909161ab
dquinoneslopez <dquinoneslopez@gmail.com>
2021-05-05 17:57:22 +0200

    Ion-modal.

---
+++ src/app/pages/modal-info/modal-info.page.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal-info',
+  templateUrl: './modal-info.page.html',
+  styleUrls: ['./modal-info.page.scss'],
+})
+export class ModalInfoPage implements OnInit {
+  
+  @Input() name: string;
+  @Input() country: string;
+  
+  constructor(
+    private modalController: ModalController
+  ) { }
+
+  ngOnInit() {
+  }
+  
+  exitWhithoutArguments() {
+    this.modalController.dismiss();
+  }
+
+  exitWhithArguments() {
+    this.modalController.dismiss({
+      name: 'Felipe',
+      country: 'Perú'
+    });
+  }
+}
